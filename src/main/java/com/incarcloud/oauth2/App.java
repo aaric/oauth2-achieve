cf36e6c1de92cdce1af914969165768eb332b143
qlhan <qlhan@incarcloud.com>
2020-03-11 16:18:48 +0800

    0.0.1-SNAPSHOT innit

---
+++ src/main/java/com/incarcloud/oauth2/App.java
@@ -0,0 +1,23 @@
+package com.incarcloud.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动类
+ *
+ * @author Aaric, created on 2020-03-11T16:06.
+ * @version 0.0.1-SNAPSHOT
+ */
+@SpringBootApplication
+public class App {
+
+    /**
+     * Main
+     *
+     * @param args 命令行参数
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
