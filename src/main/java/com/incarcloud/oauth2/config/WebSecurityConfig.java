6c307fcd79f46dcd6f07d7567214f1083dbda40e
qlhan <qlhan@incarcloud.com>
2020-03-15 17:28:48 +0800

    0.2.2-SNAPSHOT rbac jdbc

--- src/main/java/com/incarcloud/oauth2/config/WebSecurityConfig.java
+++ src/main/java/com/incarcloud/oauth2/config/WebSecurityConfig.java
@@ -1,11 +1,13 @@
 package com.incarcloud.oauth2.config;
 
+import com.incarcloud.oauth2.security.service.impl.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -25,6 +27,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         //return NoOpPasswordEncoder.getInstance();
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
     }
 
     @Override
