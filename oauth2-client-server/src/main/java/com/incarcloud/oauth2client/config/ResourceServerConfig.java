836729d36ef4c06f82394cba4c32900625e078b8
qlhan <qlhan@incarcloud.com>
2020-03-16 11:04:38 +0800

    0.3.0-SNAPSHOT oauth2 client

--- oauth2-client-server/src/main/java/com/incarcloud/oauth2client/config/ResourceServerConfig.java
+++ oauth2-client-server/src/main/java/com/incarcloud/oauth2client/config/ResourceServerConfig.java
@@ -3,28 +3,28 @@ package com.incarcloud.oauth2client.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 /**
- * 安全认证配置
+ * 资源认证配置
  *
- * @author Aaric, created on 2020-03-15T23:02.
+ * @author Aaric, created on 2020-03-16T10:35.
  * @version 0.3.0-SNAPSHOT
  */
 @Configuration
-@EnableWebSecurity
+@EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
         http.exceptionHandling()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/**").hasAuthority("SystemUser");
+                .antMatchers("/api/oauth2client/**").hasAuthority("oauth2client:admin");
     }
 }
