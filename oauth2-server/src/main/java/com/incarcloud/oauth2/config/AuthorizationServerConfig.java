f055ec7cb820cd3945075b437717e87a01e1c3c1
qlhan <qlhan@incarcloud.com>
2020-03-22 18:04:36 +0800

    0.6.0-SNAPSHOT jwt hs256 client

--- oauth2-server/src/main/java/com/incarcloud/oauth2/config/AuthorizationServerConfig.java
+++ oauth2-server/src/main/java/com/incarcloud/oauth2/config/AuthorizationServerConfig.java
@@ -19,7 +19,6 @@ import org.springframework.security.oauth2.provider.client.JdbcClientDetailsServ
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import javax.sql.DataSource;
 
