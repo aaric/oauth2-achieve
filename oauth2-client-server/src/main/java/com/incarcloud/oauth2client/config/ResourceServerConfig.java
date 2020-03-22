db10c3d1002afdf40310529116204cfac6e890ec
qlhan <qlhan@incarcloud.com>
2020-03-22 18:04:12 +0800

    0.6.0-SNAPSHOT jwt hs256 client

--- oauth2-client-server/src/main/java/com/incarcloud/oauth2client/config/ResourceServerConfig.java
+++ oauth2-client-server/src/main/java/com/incarcloud/oauth2client/config/ResourceServerConfig.java
@@ -1,9 +1,14 @@
 package com.incarcloud.oauth2client.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 /**
  * 资源认证配置
@@ -16,6 +21,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    // 0.3.0-SNAPSHOT
     /*@Override
     public void configure(HttpSecurity http) throws Exception {
         http.exceptionHandling()
@@ -25,4 +31,21 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/oauth2client/**").hasAuthority("admin:oauth2client");
     }*/
+
+    @Bean
+    public JwtAccessTokenConverter jwtTokenEnhancer() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey("oauth2");
+        return jwtAccessTokenConverter;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtTokenEnhancer());
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore());
+    }
 }
